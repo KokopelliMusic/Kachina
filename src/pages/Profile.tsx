f51d8878ad0e5089863855fe1f2e0ed4be4db18d
Nierot <git@nierot.com>
2022-01-14 14:25:38 +0100

    🎉 Begin working on pages

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,26 @@
+import { List, ListItem, ListItemText, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import React, { useEffect, useState } from 'react'
+
+const Profile = () => {
+  const [username, setUsername] = useState<string>()
+
+
+  useEffect(() => {
+    window.sipapu.getUsername()
+      .then(name => setUsername(name))
+      .catch(err => {
+        alert('fucking error handling')
+      })
+  })
+
+  return <Box>
+    <Typography
+      variant="h3"
+      component="h3">
+      Hello, {username}!
+    </Typography>
+  </Box>
+}
+
+export default Profile
---
+++ src/pages/Session.tsx
@@ -0,0 +1,17 @@
+import { Box } from '@mui/system'
+import React, { useEffect, useState } from 'react'
+
+const Session = () => {
+  const [session, setSession] = useState()
+
+
+  useEffect(() => {
+    // window.sipapu.Session.get
+  })
+
+  return <Box>
+    Session
+  </Box>
+}
+
+export default Session
