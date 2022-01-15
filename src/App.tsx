7b9e3cf01fe672c56f91e1bf63b96d85666c56b6
Niels <56843328+Nierot@users.noreply.github.com>
2022-01-15 16:05:08 +0100

    🏗️ Change app into 3 routers

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,24 @@ import React  from 'react'
 import AnonRouter from './routers/AnonRouter'
 import AuthRouter from './routers/AuthRouter'
 import { HashRouter } from 'react-router-dom'
+import SessionRouter from './routers/SessionRouter'
 
 function App() {
+  let sessionPath = false
+
+  if (window.location.pathname.startsWith('/session')) {
+    sessionPath = true
+  }
+
   return <HashRouter>
-    {window.sipapu.isLoggedIn() ? <AuthRouter /> : <AnonRouter />}
+    {window.sipapu.isLoggedIn() ?
+      (sessionPath ? 
+        <SessionRouter /> : 
+        <AuthRouter />) :
+      <AnonRouter />
+    }
   </HashRouter>
+
 }
 
 export default App
