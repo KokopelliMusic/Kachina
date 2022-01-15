0bd2b9078de00c924834c754b91828c85aaf0911
Niels <56843328+Nierot@users.noreply.github.com>
2022-01-15 16:12:10 +0100

    📝 Setup scaffolding for sessioncreation

---
+++ src/pages/auth/SessionCreation.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Box } from '@mui/system'
+
+// 1. Select playlist (or create new one)
+// 2. Session settings (eg. use of spotify)
+// 3. if user is already logged into spotify then skip this step, else let user log into spotify (and ask if they want to stay logged in after session is donezo)
+// 4. connect to tv
+// 5. redirect to /session/session
+
+const SessionCreation = () => {
+
+  return <Box>
+    aaaa
+  </Box>
+}
+
+export default SessionCreation
