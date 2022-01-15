803ca14fbcbb938d34f7dc6bdae17d6209ec67e1
Niels <56843328+Nierot@users.noreply.github.com>
2022-01-15 16:11:47 +0100

    ⚡️ Let SessionCreation use the new redirect hook

--- src/pages/auth/SessionSelect.tsx
+++ src/pages/auth/SessionSelect.tsx
@@ -1,18 +1,25 @@
 import { Button, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router'
 import OTP from '../../components/OTP'
+import useRedirect from '../../components/Redirect'
 import { useNotification } from '../../components/Snackbar'
+import { saveSessionCode } from '../../data/session'
 
 const SessionSelect = () => {
   const [code, setCode] = useState<string>()
   const [notify, Snackbar] = useNotification()
 
+  const navigate = useNavigate()
+  const redirect = useRedirect()
+
   const joinSession = () => {
     if (code && code.length === 4) {
       window.sipapu.Session.join(code)
         .then(() => {
-          alert('Joined session')
+          saveSessionCode(code)
+          navigate('/session/session')
         })
         .catch(err => {
           if (err.message === 'session is undefined') {
@@ -24,42 +31,45 @@ const SessionSelect = () => {
     }
   }
 
-  return <Box>
-    <Snackbar />
+  const createSession = () => redirect('/sessionCreation')
 
-    <Typography
-      className="text-center w-full pt-4 px-8"
-      variant="h4"
-      component="h1">
-      Get started with Kokopelli
-    </Typography>
+  return <Box className="h-4/6 flex justify-center items-center">
+    <div>
+      <Snackbar />
 
-    <Typography
-      className="text-center w-full pt-4 px-8"
-      component="h2">
-      Fill in the session code of an existing session...
-    </Typography>
+      <Typography
+        className="text-center w-full pt-4 px-8"
+        variant="h4"
+        component="h1">
+        Get started with Kokopelli
+      </Typography>
 
-    <OTP setCode={setCode}/>
+      <Typography
+        className="text-center w-full pt-4 px-8"
+        component="h2">
+        Fill in the session code of an existing session...
+      </Typography>
 
-    <div className="center w-full pt-6 pb-4">
-      <Button variant="contained" onClick={joinSession}>
-        Join session
-      </Button>
+      <OTP setCode={setCode}/>
+
+      <div className="center w-full pt-6 pb-4">
+        <Button variant="contained" onClick={joinSession}>
+          Join session
+        </Button>
+      </div>
+
+      <Typography
+        className="text-center w-full pt-4"
+        component="h2">
+        ...or create a new session
+      </Typography>
+
+      <div className="center w-full pt-6">
+        <Button variant="contained" onClick={createSession}>
+          Create new session
+        </Button>
+      </div>
     </div>
-
-    <Typography
-      className="text-center w-full pt-4"
-      component="h2">
-      ...or create a new session
-    </Typography>
-
-    <div className="center w-full pt-6">
-      <Button variant="contained">
-        Create new session
-      </Button>
-    </div>
-
   </Box>
 }
 
