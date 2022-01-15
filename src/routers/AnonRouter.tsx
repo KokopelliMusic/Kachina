b35197c835bea3e97bc023836d14db0910ce64e3
Niels <56843328+Nierot@users.noreply.github.com>
2022-01-15 10:51:48 +0100

    ♻️ Moving around code

--- src/routers/AnonRouter.tsx
+++ src/routers/AnonRouter.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Welcome from '../pages/Welcome'
+import Welcome from '../pages/anon/Welcome'
 
 const AnonRouter = () => {
   return <Routes>
