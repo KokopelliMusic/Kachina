0bed93e02d73392d17e7b9e8fd97352f89a83320
Nierot <git@nierot.com>
2022-01-16 23:38:13 +0100

    ➕ add small TODO

--- src/pages/EditPlaylist.tsx
+++ src/pages/EditPlaylist.tsx
@@ -6,12 +6,21 @@ import { useParams } from 'react-router'
 import { PlaylistWithSongsType } from 'sipapu/dist/src/services/playlist'
 import { useNotification } from '../components/Snackbar'
 import { SongType } from 'sipapu/dist/src/services/song'
+import useRedirect from '../components/Redirect'
+import { ProfileType } from 'sipapu/dist/src/services/profile'
+import { purple } from '@mui/material/colors'
+
+// TODO AddedBy toevoegen voor elk nummer
 
 const EditPlaylist = () => {
-  const params = useParams()
-  const [notify, Snackbar] = useNotification()
-  
+  const params              = useParams()
+  const redirect            = useRedirect()
+  const [notify, Snackbar]  = useNotification()
+
   const [playlist, setPlaylist] = useState<PlaylistWithSongsType | undefined>(undefined)
+  const [users, setUsers]       = useState<ProfileType[] | undefined>(undefined)
+  const [user, setUser]          = useState<ProfileType | undefined>(undefined)
+  
 
   useEffect(() => {
     if (!params.id) {
@@ -20,17 +29,35 @@ const EditPlaylist = () => {
     }
 
     window.sipapu.Playlist.getWithSongs(parseInt(params.id))
-      .then(p => {
-        console.log(p)
-        return p
-      })
       .then(setPlaylist)
       .catch(err => {
         notify({ title: 'Error', message: err.message, severity: 'error' })
       })
 
-    
+    window.sipapu.Profile.getCurrent()
+      .then(setUser)
+      .catch(err => {
+        notify({ title: 'Error', message: err.message, severity: 'error' })
+      })
   }, [])
+
+  useEffect(() => {
+    if (!playlist) return
+
+    window.sipapu.Playlist.getUsers(playlist.id)
+      .then(u => {
+        console.log(u)
+        return u
+      })
+      .then(setUsers)
+      .catch(err => {
+        notify({ title: 'Error', message: err.message, severity: 'error' })
+      })
+  }, [playlist])
+ 
+  // This is safe to do since we already check it in useEffect, that is before render so its safe
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const clickAdd = () => redirect('/add/' + parseInt(params.id!))
 
   // TODO
   if (!playlist) {
@@ -49,15 +76,27 @@ const EditPlaylist = () => {
     <Snackbar />
     
     <main className="mb-8 scroll">
-      <Typography
-        className="center w-full py-4"
-        variant="h2">
-        {playlist.name}
-      </Typography>
-      <Typography
-        className="center w-full pb-4">
-        Made by {'TODO'}
-      </Typography>
+      <div className="center w-full">
+        <Typography
+          className="w-11/12 text-center pt-4 pb-2"
+          noWrap
+          variant="h4">
+          {playlist.name}
+        </Typography>
+      </div>
+
+      <div className="w-full flex justify-center items-center pb-4">
+        <Avatar src={user?.profilePicture} alt={user?.username} sx={{ bgcolor: purple[500], width: 36, height: 36 }}>
+          {user?.username.substring(0, 1)}
+        </Avatar>
+
+        <div className="ml-2" />
+
+        <Typography
+          className="h-full">
+          Made by {!user ? '...' : user.username}
+        </Typography>
+      </div>
 
       <div className="w-full center pb-4">
         <Divider className="w-80"/>
@@ -68,8 +107,10 @@ const EditPlaylist = () => {
       </List>
     </main>
 
+    <div className="pb-20" />
+
     <div className="fixed bottom-20 right-6">
-      <Fab color="primary" onClick={() => notify({ title: 'Error', message: 'Adding songs is not supported yet', severity: 'error'})}>
+      <Fab color="primary" onClick={clickAdd}>
         <Add />
       </Fab>
     </div>
@@ -89,7 +130,7 @@ const SongItem = ({ song }: SongItemProps) => {
       <Avatar variant="square" alt={song.title} src={cover} />
     </ListItemAvatar>
 
-    <ListItemText primary={song.title} secondary={artist} primaryTypographyProps={{ noWrap: true }} />
+    <ListItemText primary={song.title} secondary={artist + ' | TODO: addedBy'} primaryTypographyProps={{ noWrap: true }} />
   </ListItem>
 }
 
