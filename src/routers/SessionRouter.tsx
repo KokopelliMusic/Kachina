a59a6db880a99caa0b3f4c1eb174616059eb59b1
Niels <56843328+Nierot@users.noreply.github.com>
2022-01-22 13:27:42 +0100

    🎨 Change playlists -> playlist

--- src/routers/SessionRouter.tsx
+++ src/routers/SessionRouter.tsx
@@ -8,7 +8,6 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 import { Box } from '@mui/system'
 import { Link, Route, Routes } from 'react-router-dom'
 import NotFound from '../pages/NotFound'
-import Playlists from '../pages/Playlists'
 import Session from '../pages/session/Session'
 import Settings from '../pages/session/Settings'
 import Add from '../pages/session/Add'
@@ -16,7 +15,7 @@ import Profile from '../pages/Profile'
 import useRedirect from '../components/Redirect'
 
 const pages = {
-  playlists: <Playlists />,
+  playlist: <p>TODO: Playlist view</p>,
   session: <Session />,
   add: <Add />,
   settings: <Settings />,
@@ -64,12 +63,12 @@ const SessionRouter = () => {
 
     <div className="h-full overflow-scroll" style={{ marginTop: '64px' }}>
       <Routes>
-        <Route path="/session/playlists" element={pages.playlists} />
-        <Route path="/session/session" element={pages.session} />
-        <Route path="/session/add" element={pages.add} />
-        <Route path="/session/profile" element={pages.profile} />
-        <Route path="/session/settings" element={pages.settings} />
-        <Route path="/session/*" element={pages.notFound} />
+        <Route path="session/playlist" element={pages.playlist} />
+        <Route path="session/session" element={pages.session} />
+        <Route path="session/add" element={pages.add} />
+        <Route path="session/profile" element={pages.profile} />
+        <Route path="session/settings" element={pages.settings} />
+        <Route path="session/*" element={pages.notFound} />
       </Routes>
     </div>
 
@@ -80,10 +79,10 @@ const SessionRouter = () => {
         className="fixed bottom-0 left-0 right-0"
       >
         <BottomNavigationAction 
-          label="Playlists"
+          label="Playlist"
           component={Link}
-          to="/session/playlists"
-          value={'playlists'} 
+          to="/session/playlist"
+          value={'playlist'} 
           icon={<QueueMusicIcon />} />
         <BottomNavigationAction 
           label="Session"
