deafc4b79f6c0dee170aac32bc4689b38213d5a4
Nierot <git@nierot.com>
2022-02-18 13:06:56 +0100

    ✨ Add placeholder settings page

---
+++ src/pages/Settings.tsx
@@ -0,0 +1,19 @@
+import { Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import React from 'react'
+
+const Settings = () => {
+  return <Box className="p-4">
+    <Typography
+      variant="h2">
+      Settings
+    </Typography>
+
+    <Typography
+      variant="h5">
+      TODO
+    </Typography>
+  </Box>
+}
+
+export default Settings
