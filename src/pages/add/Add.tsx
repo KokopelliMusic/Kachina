cadb73b625f076130c31eb80cb6aabe7187a0cdb
Nierot <git@nierot.com>
2022-02-18 13:06:21 +0100

    🎨 Add warning for YouTube

--- src/pages/add/Add.tsx
+++ src/pages/add/Add.tsx
@@ -4,6 +4,7 @@ import { Box } from '@mui/system'
 import useRedirect from '../../components/Redirect'
 import { useParams } from 'react-router'
 import { getSessionCode } from '../../data/session'
+import { useNotification } from '../../components/Snackbar'
 
 type AddProps = {
   session?: boolean
@@ -12,6 +13,8 @@ type AddProps = {
 const Add = ({ session }: AddProps) => {
   const redirect = useRedirect()
   const params   = useParams()
+
+  const [notify, Snackbar] = useNotification()
 
   const [id, setId] = useState<number>(-1)
   
@@ -34,6 +37,7 @@ const Add = ({ session }: AddProps) => {
   })
 
   return <Box className="h-4/6 flex items-center">
+    <Snackbar />
     <div>
       <Typography
         className="text-center pt-4 px-2"
@@ -50,11 +54,11 @@ const Add = ({ session }: AddProps) => {
       <main className="w-full h-1/2">
 
         <div className="center w-full py-8">
-          <Button variant="contained" className="w-1/2 mx-2" onClick={() => redirect('/spotify/' + id)}>Spotify</Button>
+          <Button variant="contained" className="w-1/2 mx-2" onClick={() => redirect('/add/spotify/' + id)}>Spotify</Button>
         </div>
 
         <div className="center w-full">
-          <Button variant="contained" className="w-1/2" onClick={() => redirect('/youtube/' + id)}>YouTube</Button>
+          <Button variant="contained" className="w-1/2" onClick={() => notify({ title: 'Oops', message: 'Sorry, YouTube functionality has not been implemented yet', severity: 'warning' })}>YouTube</Button>
         </div>
 
       </main>
