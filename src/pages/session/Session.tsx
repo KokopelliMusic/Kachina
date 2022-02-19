a34540871853e2e562f5edd2de8b55fb90e3a236
Niels <56843328+Nierot@users.noreply.github.com>
2022-02-19 18:51:47 +0100

    🎨 Make the session screen more beautiful and make it react to events

--- src/pages/session/Session.tsx
+++ src/pages/session/Session.tsx
@@ -1,142 +1,237 @@
 import { Box } from '@mui/system'
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useContext, useEffect, useMemo, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { SessionType } from 'sipapu/dist/src/services/session'
 import { useNotification } from '../../components/Snackbar'
-import { getSessionCode } from '../../data/session'
 import SkipNextIcon from '@mui/icons-material/SkipNext'
 import SkipPreviousIcon from '@mui/icons-material/SkipPrevious'
 import PauseCircleIcon from '@mui/icons-material/PauseCircle'
 import HistoryIcon from '@mui/icons-material/History'
+import PlayCircleIcon from '@mui/icons-material/PlayCircle'
 import QueueMusicIcon from '@mui/icons-material/QueueMusic'
 import useRedirect from '../../components/Redirect'
-import { EventTypes, NextSongEventData } from 'sipapu/dist/src/events'
-import { SongEnum, SongType } from 'sipapu/dist/src/services/song'
-import { Button, Typography } from '@mui/material'
+import { EventTypes } from 'sipapu/dist/src/events'
+import { SongType } from 'sipapu/dist/src/services/song'
+import { Button, Typography, Skeleton } from '@mui/material'
 import PlayerProgressionBar from '../../components/PlayerProgressionBar'
+import Kokopelli from '../../components/Kokopelli'
+import { EventContext } from '../../routers/SessionRouter'
 
 const Session = () => {
   const [notify, Snackbar] = useNotification()
   const navigate           = useNavigate()
   const redirect           = useRedirect()
+  const event              = useContext(EventContext)
   
-  const [code, setCode]       = useState<string>('')
-  const [session, setSession] = useState<SessionType>()
-  
+  const [loading, setLoading]   = useState<boolean>(false)
+  const [session, setSession]   = useState<SessionType>()
   const [song, setSong]         = useState<SongType>()
   const [progress, setProgress] = useState<number>(77)
-
-  useEffect(() => {
-    const code = getSessionCode()
-
-    if (!code) {
-      notify({ title: 'Catastophic Failure', message: 'The session is invalid or expired, sending you back to home in 5 seconds', severity: 'error' })
-      setTimeout(() => navigate('/auth/session'), 5000)
-      return
-    }
-
-    const close = window.sipapu.Session.watch(code, event => {
-      if (event.eventType === EventTypes.NEXT_SONG) {
-        const data = event.data as NextSongEventData
-        setSong(data.song)
-      }
-    })
-
-    // TODO
-    setSong({
-      id: 1,
-      title: 'Hallowed Be Thy Name - 2015 Remaster',
-      artist: 'Iron Maiden',
-      cover: 'https://i.scdn.co/image/ab67616d0000b2735c29a88ba5341ca428f0c322',
-      addedBy: 'Niels',
-      createdAt: new Date(),
-      playCount: 0,
-      queryResult: {},
-      songType: SongEnum.SPOTIFY,
-      platformId: '',
-      playlistId: 29,
-    })
-
-    setCode(code)
-    // close the connection with the backend
-    return () => {
-      const fun = async () => (await close)()
-      fun()
-    }
-  }, [])
-
-  useMemo(() => {
-    window.sipapu.Session.get(code)
-      .then(setSession)
-      .catch(() => {
-        notify({ title: 'Session not found', message: 'The session code you entered is invalid', severity: 'error' })
-      })
-  }, [code])
+  const [playing, setPlaying]   = useState<boolean>(false)
 
   const skip = () => {
-    window.sipapu.Session.notifyEvent(code, EventTypes.SKIP_SONG, {})
+    window.sipapu.Session.notifyEvent(event.session, EventTypes.SKIP_SONG, {})
   }
 
   const playPause = () => {
-    window.sipapu.Session.notifyEvent(code, EventTypes.PLAY_PAUSE, {})
+    window.sipapu.Session.notifyEvent(event.session, EventTypes.PLAY_PAUSE, {})
   }
 
   const previous = () => {
-    window.sipapu.Session.notifyEvent(code, EventTypes.PREVIOUS_SONG, {})
+    window.sipapu.Session.notifyEvent(event.session, EventTypes.PREVIOUS_SONG, {})
   }
 
   const history = () => redirect('/history')
   const queue = () => redirect('/queue')
 
-  return <Box className="h-screen overflow-scroll">
-    <Snackbar />
-    <Box className="grid grid-rows-9 h-full pb-32">
+  useEffect(() => {
+    // TODO pak het nummer van de cache ofzo
+    switch (event.eventType) {
 
-      {/* Queue and History buttons */}
-      <Box className="w-full flex justify-between py-2 px-4 row-span-1">
-        <Button onClick={history}> <HistoryIcon    sx={{ fontSize: 35 }} /> </Button>
-        <Button onClick={queue}>   <QueueMusicIcon sx={{ fontSize: 35 }} /> </Button>
-      </Box>
+    case EventTypes.PLAY_SONG:
+      setPlaying(true)
+      setSong((JSON.parse(event.data as unknown as string).song) as SongType)
+      break
+      
+    case EventTypes.NEXT_SONG:
+      setPlaying(true)
+      setSong((JSON.parse(event.data as unknown as string).song) as SongType)
+      setProgress(0)
+      break
 
-      {/* Album cover */}
-      <Box className="px-8 row-span-2">
-        <img src={song?.cover ?? '/missing.jpg'} />    
-      </Box>
+    
+    case EventTypes.PREVIOUS_SONG:
+    case EventTypes.SONG_FINISHED:
+    case EventTypes.SKIP_SONG:
+      setPlaying(false)
+      setProgress(0)
+      break
 
-      <Box className="px-8 pt-6 w-screen h-full row-span-3">
-        {/* Song information */}
-        <Box>
-          <Typography
-            noWrap
-            variant="h5">
-            {song?.title ?? ''}
-          </Typography>
+    case EventTypes.PLAY_PAUSE:
+      setPlaying(!playing)
+      break
 
-          <Typography
-            noWrap
-            variant="body1">
-            {song?.artist ?? ''}
-          </Typography>
+    default:
+      // Do nothing by default
+      // This is so we do not trigger the useMemo below, which would cause a re-render
+      break
 
-        </Box>
+    }
+  }, [event])
 
-        {/* Progress bar */}
-        <Box className="pt-2">
-          <PlayerProgressionBar timeLeft={40} />
-        </Box>
+  useEffect(() => {
+    if (song?.length) {
+      const interval = setInterval(() => {
+        if (song?.length) {
+          setProgress(p => p + 1000)
+        }
+      }, 1000)
 
-        {/* Player buttons */}
-        <Box className="w-full center">
-          <Box>
-            <Button onClick={previous}>  <SkipPreviousIcon sx={{ fontSize: 40 }}/></Button>
-            <Button onClick={playPause}> <PauseCircleIcon  sx={{ fontSize: 50 }}/></Button>
-            <Button onClick={skip}>      <SkipNextIcon     sx={{ fontSize: 40 }}/></Button>
+      return () => {
+        clearInterval(interval)
+      }
+    }
+  }, [playing])
+
+  // useMemo prevents a rerender when any event fires, this makes everything a lot quicker
+  return useMemo(() => {
+    if (loading) {
+      return <Box className="h-screen">
+        <Snackbar />
+        <Box className="grid grid-rows-9 h-full pb-32">
+          {/* History buttons */}
+          <Box className="w-full flex justify-between py-2 px-4 row-span-1">
+            <Button> <Skeleton variant="circular" width={35} height={35} /> </Button>
+            <Button> <Skeleton variant="circular" width={35} height={35} /> </Button>
+          </Box>
+  
+          {/* Album cover */}
+          <Box className="px-8 row-span-2">
+            <Skeleton variant="rectangular">
+              <img src='/missing.jpg' />
+            </Skeleton>
+          </Box>
+  
+          {/* Song info */}
+          <Box className="px-8 pt-6 w-screen h-full row-span-3">
+            <Box>
+              <Skeleton>
+                <Typography noWrap variant="h4">
+                  This is a very long song title that should be truncated
+                </Typography>
+              </Skeleton>
+    
+              <Skeleton>
+                <Typography variant="body1">
+                  The songs artist is a bit shorter
+                </Typography>
+              </Skeleton>
+            </Box>
+  
+            {/* Progress bar */}
+            <Box className="pt-2 pb-2 pt-4">
+              <Skeleton className="w-full" height={20}/>
+            </Box>
+  
+            {/* Player buttons */}
+            <Box className="w-full center">
+              <Box className="flex items-center space-x-4">
+                <Skeleton variant="circular" width={40} height={40} />
+                <Skeleton variant="circular" width={50} height={50} />
+                <Skeleton variant="circular" width={40} height={40} />
+              </Box>
+            </Box>
           </Box>
         </Box>
       </Box>
-
+    }
+  
+    if (!song) {
+      return <Box className="h-screen">
+        <Snackbar />
+  
+        <Box className="grid grid-rows-4 h-full pb-32">
+  
+          <Box className="row-span-1 w-full text-center pt-4">
+            <Typography
+              variant="h4">
+              No song is playing!
+            </Typography>
+  
+            <Typography
+              className="px-4 pt-2"
+              variant="body1">
+              This could be because the playlist is empty or the player is having errors
+            </Typography>
+  
+            <Typography
+              className="px-6"
+              variant="body1">
+              To fix this, try adding some songs and/or restarting the player (press F5)
+            </Typography>
+          </Box>
+  
+          <Box className="row-span-2 center">
+            <Kokopelli />
+          </Box>
+  
+        </Box>
+      </Box>
+    }
+  
+    return <Box className="h-screen overflow-scroll">
+      <Snackbar />
+      <Box className="grid grid-rows-9 h-full pb-32">
+  
+        {/* Queue and History buttons */}
+        <Box className="w-full flex justify-between py-2 px-4 row-span-1">
+          <Button onClick={history}> <HistoryIcon    sx={{ fontSize: 35 }} /> </Button>
+          <Button onClick={queue}>   <QueueMusicIcon sx={{ fontSize: 35 }} /> </Button>
+        </Box>
+  
+        {/* Album cover */}
+        <Box className="px-8 row-span-2">
+          <img src={song?.cover ?? '/missing.jpg'} />    
+        </Box>
+  
+        <Box className="px-8 pt-6 w-screen h-full row-span-3">
+          {/* Song information */}
+          <Box>
+            <Typography
+              noWrap
+              variant="h5">
+              {song.title}
+            </Typography>
+  
+            <Typography
+              noWrap
+              variant="body1">
+              {song.artist}
+            </Typography>
+  
+          </Box>
+  
+          {/* Progress bar */}
+          <Box className="pt-2">
+            <PlayerProgressionBar timeLeft={song.length ? ((progress / song.length) * 100) : 100} />
+          </Box>
+  
+          {/* Player buttons */}
+          <Box className="w-full center">
+            <Box>
+              <Button onClick={previous}>  <SkipPreviousIcon sx={{ fontSize: 40 }}/></Button>
+              {playing ? 
+                <Button onClick={playPause}> <PauseCircleIcon  sx={{ fontSize: 50 }}/></Button> :
+                <Button onClick={playPause}> <PlayCircleIcon   sx={{ fontSize: 50 }}/></Button>
+              }
+              <Button onClick={skip}>      <SkipNextIcon     sx={{ fontSize: 40 }}/></Button>
+            </Box>
+          </Box>
+        </Box>
+  
+      </Box>
     </Box>
-  </Box>
+  }, [loading, song, playing, progress])
 }
 
 export default Session
