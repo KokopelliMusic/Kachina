79c177a4d2383cc5d40749112efcca97e3cdea4e
Niels <56843328+Nierot@users.noreply.github.com>
2022-02-19 18:50:45 +0100

    ✨ Auto delete old sessions

--- src/data/session.ts
+++ src/data/session.ts
@@ -1,8 +1,26 @@
 
 export const saveSessionCode = (code: string) => {
-  localStorage.setItem('kokopelli:sessionCode', code)
+  localStorage.setItem('kokopelli:sessionCode', JSON.stringify({
+    timeSet: (new Date()).getTime(),
+    code
+  }))
+}
+
+export const leaveSession = () => {
+  localStorage.removeItem('kokopelli:sessionCode')
+  window.location.href = '/'
 }
 
 export const getSessionCode = () => {
-  return localStorage.getItem('kokopelli:sessionCode')
+  const item = localStorage.getItem('kokopelli:sessionCode')
+
+  if (item) {
+    const obj = JSON.parse(item)
+
+    // if the session code is older than 24 hours, remove it
+    if (obj.timeSet + (1000 * 60 * 60 * 24) > (new Date()).getTime()) {
+      return obj.code
+    }
+  }
+  return null
 }
