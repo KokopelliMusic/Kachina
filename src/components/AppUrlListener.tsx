10efd688c009247e46ee9e634773e06f66d0ae52
Niels <56843328+Nierot@users.noreply.github.com>
2022-02-25 19:16:32 +0100

    ✨ Add deeplinks!

---
+++ src/components/AppUrlListener.tsx
@@ -0,0 +1,37 @@
+import { useNavigate } from 'react-router'
+import { App, URLOpenListenerEvent } from '@capacitor/app'
+import { useEffect } from 'react'
+import { createBrowserHistory } from 'history'
+
+const AppUrlListener = () => {
+  const navigate = useNavigate()
+  const history  = createBrowserHistory()
+
+  useEffect(() => {
+    App.addListener('appUrlOpen', (event: URLOpenListenerEvent) => {
+      const slug = event.url.split('.nl').pop()
+      console.log('appUrlOpen', event.url)
+      console.log('appUrlOpen href', window.location.href)
+      console.log('slug', slug)
+
+      if (slug?.startsWith('/?code')) {
+        const code = slug.split('?code=')[1].split('#')[0]
+        const hash = slug.split('#')[1]
+        const dec = decodeURIComponent(hash)
+        console.log(dec + '&code=' + code)
+        window.location.reload()
+      } else {
+        if (slug) {
+          navigate(slug)
+          window.location.reload()
+        }
+      }
+
+
+    })
+  }, [])
+
+  return null
+}
+
+export default AppUrlListener
